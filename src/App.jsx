90f8b4f55deb2eb021056c785cc8e6d4783115e0
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-09-27 12:00:02 +0300

    Kurssien määrä voi olla mikä vain. Css päivitetty.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 
 const Course = (props) => {
+  console.log("props", props);
+
   return (
     <>
-      <Header name={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
+      {props.courses.map((course) => (
+        <>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+          <div id="borderline"></div>
+        </>
+      ))}
     </>
   );
 };
@@ -20,9 +27,6 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  console.log("props in Content (toivottavasti vainn parts?)");
-  console.log(props);
-
   return (
     <ul>
       {props.parts.map((part) => (
@@ -55,36 +59,70 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+    {
+      name: "Anskun sirkuskurssi",
+      id: 3,
+      parts: [
+        {
+          name: "Käsinseisonta",
+          exercises: 2,
+          id: 1,
+        },
+        {
+          name: "Jongleeraus",
+          exercises: 1,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      <Course courses={courses} />
     </div>
   );
 };
